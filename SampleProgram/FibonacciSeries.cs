93b27d473e504b20291ced77223a47e42a01bf6f
Navinda Chandrasiri <adilanga@gmail.com>
2023-02-13 22:01:06 +0530

    Fibonacci Series

---
+++ SampleProgram/FibonacciSeries.cs
@@ -0,0 +1,26 @@
+﻿namespace CSharp_Learning.SampleProgram;
+
+public class FibonacciSeries
+{
+    public static void Run()
+    {
+        int n1 = 0, n2 = 1, n3, i, num;
+        
+        // Taking input for elements
+        Console.Write("Enter the number of elements you want: ");
+        num = int.Parse(Console.ReadLine());
+        
+        // Printing first two elements
+        Console.Write(n1 + " " + n2 + " ");
+        
+        // Printing from third element
+        for (i = 2; i < num; i++)
+        {
+            n3 = n1 + n2;
+            Console.Write(n3 + " ");
+
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+}
---
+++ SampleProgram/FibonacciSeriesRecursion.cs
@@ -0,0 +1,28 @@
+﻿namespace CSharp_Learning.SampleProgram;
+
+public class FibonacciSeriesRecursion
+{
+    static int fibonacci(int num)
+    {
+        if (num <= 1)
+        {
+            return num;
+        }
+
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+
+    public static void Run()
+    {
+        int number;
+        
+        // Taiking input number for elements
+        Console.Write("Enter number of elements you want: ");
+        number = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < number; i++)
+        {
+            Console.Write(fibonacci(i) + " ");
+        }
+    }
+}
